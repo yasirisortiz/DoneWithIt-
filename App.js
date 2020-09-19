c3d456946e16974837edfa5282646805250dc988
Yasiris Ortiz <yasirisortiz24@gmail.com>
2020-09-19 17:06:56 -0400

    view image screen added

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen'; // importing our welcome screen
 
 export default function App() {
   return (
     //render screens here 
-    <WelcomeScreen/>
+    <ViewImageScreen/>
   );
 }
 
